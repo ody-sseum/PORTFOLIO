d16bf47256bc68b8b67dbf4ed53cb4e98a388b67
ody-sseum <odysseum.main@gmail.com>
2024-08-19 15:02:41 +0800

    19/08/24 - yipee on scroll event

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 var nav_lst = document.querySelectorAll("nav > li");
+var sec_lst = document.querySelectorAll("main > section");
+var main_sec = document.querySelector("main");
+var sec_height = [];
 var sections = ["profile", "portfolio", "contact"];
 
 nav_lst.forEach((li, index) => {
@@ -15,4 +18,19 @@ function moveToSection(location) {
     document.getElementById(location).scrollIntoView({behavior: 'smooth'});
 };
 
-console.log(nav_lst)
+main_sec.addEventListener("scroll", function(){
+    sec_lst.forEach((section, index) => {
+        var rect = section.getBoundingClientRect();
+        var sec_top = rect.top;
+        var sec_bot = rect.bottom;
+
+        var isVisible = (sec_top <= window.innerHeight) && (sec_bot > 0);
+
+        if(isVisible){
+            var active_li = document.querySelector("li.active");
+
+            active_li.classList.remove("active");
+            nav_lst[index].classList.toggle("active");
+        }
+    });
+});
